83eb952c89d659fac3464eebcefc2aede00b0ef3
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2024-12-14 16:48:46 -0800

    made the back button work properly and also made it so it shows the equation in real time

--- calculator.js
+++ calculator.js
@@ -1,10 +1,27 @@
 const input = document.getElementById("calcInput");
+const answer = document.getElementById("answerBox");
+const errorMessage = "Enter a valid equation";
 
 function appendValue(value) {
     input.value += value;
     console.log(value);
+    output();
 }
 
 function back() {
     input.value = input.value.slice(0, -1);
+    output();
+}
+
+function output() {
+    try {
+        if (eval(input.value) == "") {
+            answer.textContent = "Enter an equation";
+        } else {
+            answer.textContent = `${eval(input.value)}`;
+        }
+    }
+    catch(error) {
+        answer.textContent = errorMessage;
+    }
 }
